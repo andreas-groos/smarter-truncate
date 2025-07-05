79a45c0b0784283f8931c4cbfceb737ab267cbfd
Andreas Groos <andreas.groos1@gmail.com>
2025-07-05 07:16:22 -0700

    fixes and successful publishing

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,15 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   rules: {
+    // Only keep essential rules for code quality
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
     'prefer-const': 'error',
     'no-var': 'error',
   },
